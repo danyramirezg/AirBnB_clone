488dfe8942150141b9d171c067fc9b59fa02f971
dany <danyramirezg@gmail.com>
2019-11-08 10:45:01 -0500

    Add more methods

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,21 @@ class BaseModel:
     """Class BaseModel"""
 
     def __init__(self):
-        """Constructor"""
+        """Constructor init"""
+
         self.id = str(uuid.uuid4())
+        self.created_at = datetime
+        self.update_at = datetime
+
+    def __str__(self):
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
+        print("[{}] ({}) {}".format(classmethod, id, __dict__))
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime """
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        
+
 
