4e7c47f9c289a046bf0890871ac504d80b454eee
dany <danyramirezg@gmail.com>
2019-11-08 11:40:38 -0500

    Create base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Module Base Model"""
 
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
@@ -11,19 +11,24 @@ class BaseModel:
     def __init__(self):
         """Constructor init"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime
-        self.update_at = datetime
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """should print: [<class name>] (<self.id>) <self.__dict__>"""
-        print("[{}] ({}) {}".format(classmethod, id, __dict__))
+        """Return: [<class name>] (<self.id>) <self.__dict__>"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime """
 
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
-        
 
-
+        new_dict = self.__dict__
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        return new_dict
