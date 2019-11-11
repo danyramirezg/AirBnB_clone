20c3e5c069f4ee67392155b1b60775ad2152d710
Dany <danyramirezg@gmail.com>
2019-11-11 11:15:50 -0500

    Create a class User

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Users"""
+
+
+class User(BaseModel):
+    """A class User that inherits from BaseModel"""
+
+    email = " "
+    password = " "
+    first_name = " "
+    last_name = " "
